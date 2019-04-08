64fd7f74e4da4f561802923e7b540bc462ede043
Theo Noel <theo.noel59@gmail.com>
2019-04-08 17:18:43 +0200

    initial commit

---
+++ lib/pages/auth.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+
+class AuthPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Please authenticate'),
+        ),
+        body: Center(
+          child: RaisedButton(
+            child: Text('Login'),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => ProductsPage()
+                )
+              );
+            }
+          )
+        ),
+      );
+  }
+
+}
