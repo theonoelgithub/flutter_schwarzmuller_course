cc81f2d9758dde4ca3447443f0788a06924df3ee
Theo Noel <theo.noel59@gmail.com>
2019-06-25 12:01:23 +0200

    working with forms

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import './pages/products_admin.dart';
 import './pages/products.dart';
 import './pages/product.dart';
-
+import './pages/auth.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -21,9 +21,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Map<String, String>> _products = [];
+  List<Map<String, dynamic>> _products = [];
 
-  void _addProduct(Map<String, String> product) {
+  void _addProduct(Map<String, dynamic> product) {
     setState(() {
       _products.add(product);
     });
@@ -46,8 +46,9 @@ class _MyAppState extends State<MyApp> {
           accentColor: Colors.deepOrange),
       // home: AuthPage(),
       routes: {
-        '/': (BuildContext context) => ProductsPage(_products, _addProduct, _deleteProduct),
-        '/admin': (BuildContext context) => ProductsAdminPage(),
+        '/': (BuildContext context) => AuthPage(),
+        '/products': (BuildContext context) => ProductsPage(_products),
+        '/admin': (BuildContext context) => ProductsAdminPage(_addProduct, _deleteProduct),
       },
       onGenerateRoute: (RouteSettings settings) {
         final List<String> pathElements = settings.name.split('/');
@@ -67,7 +68,7 @@ class _MyAppState extends State<MyApp> {
         return null;
       },
       onUnknownRoute: (RouteSettings settings) {
-        return MaterialPageRoute(builder: (BuildContext context) => ProductsPage(_products, _addProduct, _deleteProduct));
+        return MaterialPageRoute(builder: (BuildContext context) => ProductsPage(_products));
       },
     );
   }
--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 import './products.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _loginValue = "";
+  String _passwordValue = "";
+  bool _acceptTerms = false;
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +18,52 @@ class AuthPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Please authenticate'),
         ),
-        body: Center(
-          child: RaisedButton(
-            child: Text('Login'),
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/');
-            }
-          )
-        ),
+        body: Container(
+          margin: EdgeInsets.all(10.0),
+          child: ListView(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (String value) {
+                  setState(() {
+                    _loginValue = value;
+                  }
+                );
+              }),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                maxLines: 4,
+                onChanged: (String value) {
+                  setState(() {
+                    _passwordValue = value;
+                  }
+                );
+              }),
+              SwitchListTile(
+                value: _acceptTerms,
+                onChanged: (bool value) {
+                  setState(() {
+                    _acceptTerms = value;
+                  });
+                },
+                title: Text('Accept Terms'),
+              ),
+              SizedBox(height: 10.0),
+              RaisedButton(
+                color: Theme.of(context).accentColor,
+                textColor: Colors.white,
+                child: Text('Log in'),
+                onPressed: () {
+                  print(_loginValue);
+                  print(_passwordValue);
+                  Navigator.pushReplacementNamed(context, '/products');
+                  },
+              )
+            ],
+          ),
+        )
       );
   }
-
 }
